093520368ab7049fc2ed1c29dab30bedc1bc2982
MiguelBG12 <miguel.bg12@gmail.com>
2023-09-24 10:43:11 -0500

    This script retrieves user data and their associated tasks from a REST API and stores the data in a JSON file. The JSON file is named after the user ID and contains a structured representation of the user's tasks with details like task title, completion status, and username

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This module defines the REST API """
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Define the base URL for the REST API
+    url = "https://jsonplaceholder.typicode.com"
+
+    # Get the user ID from the command-line argument
+    user_id = sys.argv[1]
+
+    # Fetch user data and todos for the specified user ID from the API
+    user = requests.get(url + "/users/{}".format(user_id)).json()
+    todos = requests.get(url + "/todos", params={"userId": user_id}).json()
+
+    # Create a list of dictionaries containing task details for the user
+    user_todos = [{"task": todo["title"], "completed": todo["completed"],
+                   "username": user["username"]} for todo in todos]
+
+    # Create an output data dictionary with user ID as the key and user's
+    # task list as the value
+    output_data = {user_id: user_todos}
+
+    # Write the output data to a JSON file named after the user ID
+    with open(f"{user_id}.json", "w") as outfile:
+        json.dump(output_data, outfile)
