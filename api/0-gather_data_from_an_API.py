29d746552b274d91962c87f3ad51b256370456a5
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-25 20:15:01 -0500

    Script that uses a REST API to get and display information about tasks completed by a specific user

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This module defines the REST API """
+# Import the request library to make HTTP requests.
+import requests
+# Import the sys module to access command line arguments.
+import sys
+
+
+# Checks if the script is running directly and not being imported as a module.
+if __name__ == "__main__":
+    # Defines the base URL for the JSONPlaceholder API.
+    url = "https://jsonplaceholder.typicode.com"
+
+    # Makes a GET request to the API to obtain information about a specific user, using the first command
+    # line argument as the user ID.
+    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+
+    # Makes a GET request to the API to get the list of tasks, using the first command line argument as
+    # the user ID and passing it as a parameter.
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+
+    # Calculate the total number of tasks obtained.
+    total_tasks = len(todos)
+
+    # Calculate the total number of tasks and the number of completed tasks.
+    completed_tasks = sum(1 for todo in todos if todo["completed"])
+
+    # Print a message indicating the user's name and the number of completed tasks out of the total.
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), completed_tasks, total_tasks))
+
+    # Use a list comprehension to print the titles of completed tasks with indentation.
+    [print(f"\t {todo['title']}") for todo in todos if todo["completed"]]
