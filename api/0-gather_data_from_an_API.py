c1d899b7d4257845a1a03b436082cf8d157d4323
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-25 21:08:48 -0500

    Update requests, and gitignore files was added

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,12 +11,12 @@ if __name__ == "__main__":
     # Defines the base URL for the JSONPlaceholder API.
     url = "https://jsonplaceholder.typicode.com"
 
-    # Makes a GET request to the API to obtain information about a specific user, using the first command
-    # line argument as the user ID.
+    # Makes a GET request to the API to obtain information about a specific
+    # user, using the first command line argument as the user ID.
     user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
 
-    # Makes a GET request to the API to get the list of tasks, using the first command line argument as
-    # the user ID and passing it as a parameter.
+    # Makes a GET request to the API to get the list of tasks, using the first
+    # command line argument as the user ID and passing it as a parameter.
     todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
 
     # Calculate the total number of tasks obtained.
@@ -25,9 +25,10 @@ if __name__ == "__main__":
     # Calculate the total number of tasks and the number of completed tasks.
     completed_tasks = sum(1 for todo in todos if todo["completed"])
 
-    # Print a message indicating the user's name and the number of completed tasks out of the total.
+    # Print a message indicating the user's name and the number of completed
+    # tasks out of the total.
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), completed_tasks, total_tasks))
 
-    # Use a list comprehension to print the titles of completed tasks with indentation.
+    # Prints titles with indentation using list comprehension.
     [print(f"\t {todo['title']}") for todo in todos if todo["completed"]]
